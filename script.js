25a089728a1a8fee08a1fa6720c3bc552531913a
kibarke <kibarkework@gmail.com>
2024-02-03 18:00:59 -0500

    fixed api key

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log('code-crusaders')
 
-const APIKey = 32ef5c0e;
+const APIKey = '32ef5c0e';
 const city = 'New York City';
 const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=32ef5c0e';

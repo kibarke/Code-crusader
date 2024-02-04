6c716d8a05f935740ed75fa0f5916cc14c6ff55f
corbinkane <152234164+corbinkane@users.noreply.github.com>
2024-02-04 18:36:03 -0500

    added some css

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 console.log('code-crusaders')
 
-const APIKey = 32ef5c0e;
-const city = 'New York City';
-const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=32ef5c0e';
+// const APIKey = 32ef5c0e;
+// const city = 'New York City';
+// const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=32ef5c0e';
+
+// http://img.omdbapi.com/?apikey=[yourkey]&
+
+// http://www.omdbapi.com/?apikey=[yourkey]&
+
+
+// different ways the api key will work
+

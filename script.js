b4d7db93eec20930009e0c96dd9815af30909fab
kibarke <kibarkework@gmail.com>
2024-02-08 18:25:56 -0500

    added an error message in js but it's not showing

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const container = document.querySelector('.container');
 const moviesContainer = document.getElementById("movies")
 const APIKey = '32ef5c0e';
 
-
 const options = {
     method: 'GET',
     headers: {
@@ -19,18 +18,13 @@ const options = {
 // const APIKey = '32ef5c0e';
 const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=32ef5c0e';
 
-// const searchKeyword = userScore ignore this for now
 
 const tmdbKey = 'd7fd913642846f5e8a877bee8f5fdc5e';
 //const moviesContainer = document.getElementById('moviesContainer');
-//=======
 
 
 // Access Token
 // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkN2ZkOTEzNjQyODQ2ZjVlOGE4NzdiZWU4ZjVmZGM1ZSIsInN1YiI6IjY1YmVjNzJjMWRiYzg4MDE3YzFkNjJkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H6s65fxuHSM3r3Qc894efns9UoJy9Z3uLzFqo45nVMU
-
-
-
 
 
 //fetch fucntion gets movie title user seaches for
@@ -52,22 +46,15 @@ submitBtn.addEventListener('click', function () {
 // displays users input
 function displayInfo(data) {
     container.innerHTML = "";
-
-
-
     movieTitle.textContent = data.Title;
-
 
     const movieYear = document.createElement('p');
     movieYear.textContent = data.Year;
 
-
     container.append(movieYear);
-
 
     const moviePlot = document.createElement('p');
     moviePlot.textContent = data.Plot;
-
 
     container.append(moviePlot);
 
@@ -77,25 +64,6 @@ function displayInfo(data) {
         container.append(moviePoster);
     }
 }
-// let highScores = JSON.parse(localStorage.getItem('highscores')) || [];
-
-// function saveData() {
-//             // Get the data you want to save
-//             const data = inputTitle;
-
-//             // Save the data to localStorage
-//             localStorage.setItem("click", data);
-
-//             console.log("Data saved successfully!");
-//           }
-//           function updateScoreboard() {
-//             const submit = document.getElementById("submit");
-
-//             submit.innerHTML = "";
-//          }
-
-
-
 
 
 
@@ -108,13 +76,26 @@ const optionss = {
     }
 };
 
+function showErrorModal(message) {
+    const modal = document.createElement('div');
+    modal.classList.add('error-modal');
+    modal.textContent = message;
+    document.body.appendChild(modal);
+
+    // Close the modal when clicked anywhere on the document
+    document.addEventListener('click', function closeModal() {
+        modal.remove();
+        document.removeEventListener('click', closeModal);
+    });
+}
+
 fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
     .then(response => response.json())
     .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .catch(err => showErrorModal('Error fetching trending movies: ' + err.message));
 
 fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=a1c95fb64bc56eec23cea2a07c271cd0')
     .then(response => response.json())
     .then()
     .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .catch(err => showErrorModal('Error fetching weekly trending movies: ' + err.message));
